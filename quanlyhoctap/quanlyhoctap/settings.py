4eb5932dc619030ea9f3e4866f00fbef6211577b
belowacloud <hoangha27499@gmail.com>
2019-10-27 16:00:59 +0700

    part 2

--- quanlyhoctap/quanlyhoctap/settings.py
+++ quanlyhoctap/quanlyhoctap/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'appquanly.apps.AppquanlyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
